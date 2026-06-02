47b26d99b14dd703f183d9c8c9522f9ee482d3e6
yuki-inaho <yoshikawa@inaho.co>
2026-06-02 07:30:30 +0000

    Add Muon and schedule-free optimizer support

--- engine/backbone/timm_model.py
+++ engine/backbone/timm_model.py
@@ -2,6 +2,8 @@
 
 https://towardsdatascience.com/getting-started-with-pytorch-image-models-timm-a-practitioners-guide-4e77b4bf9055#0583
 """
+
+
 import torch
 from torchvision.models.feature_extraction import get_graph_node_names, create_feature_extractor
 
@@ -11,34 +13,39 @@ from ..core import register
 
 @register()
 class TimmModel(torch.nn.Module):
-    def __init__(self, \
-        name,
-        return_layers,
-        pretrained=False,
-        exportable=True,
-        features_only=True,
-        **kwargs) -> None:
-
+    def __init__(self, name, return_layers, pretrained=False, exportable=True, features_only=True, **kwargs) -> None:
         super().__init__()
 
         import timm
+
         model = timm.create_model(
-            name,
-            pretrained=pretrained,
-            exportable=exportable,
-            features_only=features_only,
-            **kwargs
+            name, pretrained=pretrained, exportable=exportable, features_only=features_only, **kwargs
         )
         # nodes, _ = get_graph_node_names(model)
         # print(nodes)
+
         # features = {'': ''}
         # model = create_feature_extractor(model, return_nodes=features)
 
-        assert set(return_layers).issubset(model.feature_info.module_name()), \
-            f'return_layers should be a subset of {model.feature_info.module_name()}'
+        assert set(return_layers).issubset(
+            model.feature_info.module_name()
+        ), f"return_layers should be a subset of {model.feature_info.module_name()}"
 
-        # self.model = model
-        self.model = IntermediateLayerGetter(model, return_layers)
+        # Create a mapping to replace layer names written in feature_info with layer names in PyTorch
+        feature_to_child_map = {}
+        for m_name in model.feature_info.module_name():
+            # Example: 'stages.0' -> 'stages_0'
+            #          'stages.1' -> 'stages_1' ...
+            mapped_name = m_name.replace(".", "_")
+            feature_to_child_map[m_name] = mapped_name
+
+        # Replace 'stages.0', 'stages.1', etc. in the received return_layers
+        # with 'stages_0', 'stages_1', etc. and pass them to IntermediateLayerGetter
+        new_return_layers = []
+        for layer in return_layers:
+            new_return_layers.append(feature_to_child_map[layer])
+
+        self.model = IntermediateLayerGetter(model, return_layers=new_return_layers)
 
         return_idx = [model.feature_info.module_name().index(name) for name in return_layers]
         self.strides = [model.feature_info.reduction()[i] for i in return_idx]
@@ -52,9 +59,8 @@ class TimmModel(torch.nn.Module):
         return outputs
 
 
-if __name__ == '__main__':
-
-    model = TimmModel(name='resnet34', return_layers=['layer2', 'layer3'])
+if __name__ == "__main__":
+    model = TimmModel(name="resnet34", return_layers=["layer2", "layer3"])
     data = torch.rand(1, 3, 640, 640)
     outputs = model(data)
 
--- engine/solver/det_solver.py
+++ engine/solver/det_solver.py
@@ -21,8 +21,20 @@ from ..optim.lr_scheduler import FlatCosineLRScheduler
 
 class DetSolver(BaseSolver):
 
+    def _set_optimizer_mode(self, mode: str) -> None:
+        optimizer = getattr(self, 'optimizer', None)
+        mode_fn = getattr(optimizer, mode, None)
+        if callable(mode_fn):
+            mode_fn()
+
+    def _save_state(self, checkpoint_path) -> None:
+        self._set_optimizer_mode('eval')
+        dist_utils.save_on_master(self.state_dict(), checkpoint_path)
+        self._set_optimizer_mode('train')
+
     def fit(self, ):
         self.train()
+        self._set_optimizer_mode('train')
         args = self.cfg
 
         n_parameters, model_stats = stats(self.cfg)
@@ -44,6 +56,7 @@ class DetSolver(BaseSolver):
         # evaluate again before resume training
         if self.last_epoch > 0:
             module = self.ema.module if self.ema else self.model
+            self._set_optimizer_mode('eval')
             test_stats, coco_evaluator = evaluate(
                 module,
                 self.criterion,
@@ -52,6 +65,7 @@ class DetSolver(BaseSolver):
                 self.evaluator,
                 self.device
             )
+            self._set_optimizer_mode('train')
             for k in test_stats:
                 best_stat['epoch'] = self.last_epoch
                 best_stat[k] = test_stats[k][0]
@@ -62,6 +76,7 @@ class DetSolver(BaseSolver):
         start_time = time.time()
         start_epoch = self.last_epoch + 1
         for epoch in range(start_epoch, args.epoches):
+            self._set_optimizer_mode('train')
 
             self.train_dataloader.set_epoch(epoch)
             # self.train_dataloader.dataset.set_epoch(epoch)
@@ -91,7 +106,7 @@ class DetSolver(BaseSolver):
             )
 
             if not self.self_lr_scheduler:  # update by epoch 
-                if self.lr_warmup_scheduler is None or self.lr_warmup_scheduler.finished():
+                if self.lr_scheduler is not None and (self.lr_warmup_scheduler is None or self.lr_warmup_scheduler.finished()):
                     self.lr_scheduler.step()
 
             self.last_epoch += 1
@@ -102,9 +117,10 @@ class DetSolver(BaseSolver):
                 if (epoch + 1) % args.checkpoint_freq == 0:
                     checkpoint_paths.append(self.output_dir / f'checkpoint{epoch:04}.pth')
                 for checkpoint_path in checkpoint_paths:
-                    dist_utils.save_on_master(self.state_dict(), checkpoint_path)
+                    self._save_state(checkpoint_path)
 
             module = self.ema.module if self.ema else self.model
+            self._set_optimizer_mode('eval')
             test_stats, coco_evaluator = evaluate(
                 module,
                 self.criterion,
@@ -113,6 +129,7 @@ class DetSolver(BaseSolver):
                 self.evaluator,
                 self.device
             )
+            self._set_optimizer_mode('train')
 
             # TODO
             for k in test_stats:
@@ -132,9 +149,9 @@ class DetSolver(BaseSolver):
                     top1 = best_stat[k]
                     if self.output_dir:
                         if epoch >= self.train_dataloader.collate_fn.stop_epoch:
-                            dist_utils.save_on_master(self.state_dict(), self.output_dir / 'best_stg2.pth')
+                            self._save_state(self.output_dir / 'best_stg2.pth')
                         else:
-                            dist_utils.save_on_master(self.state_dict(), self.output_dir / 'best_stg1.pth')
+                            self._save_state(self.output_dir / 'best_stg1.pth')
 
                 best_stat_print[k] = max(best_stat[k], top1)
                 print(f'best_stat: {best_stat_print}')  # global best
@@ -143,10 +160,10 @@ class DetSolver(BaseSolver):
                     if epoch >= self.train_dataloader.collate_fn.stop_epoch:
                         if test_stats[k][0] > top1:
                             top1 = test_stats[k][0]
-                            dist_utils.save_on_master(self.state_dict(), self.output_dir / 'best_stg2.pth')
+                            self._save_state(self.output_dir / 'best_stg2.pth')
                     else:
                         top1 = max(test_stats[k][0], top1)
-                        dist_utils.save_on_master(self.state_dict(), self.output_dir / 'best_stg1.pth')
+                        self._save_state(self.output_dir / 'best_stg1.pth')
 
                 elif epoch >= self.train_dataloader.collate_fn.stop_epoch:
                     best_stat = {'epoch': -1, }
@@ -186,6 +203,7 @@ class DetSolver(BaseSolver):
         self.eval()
 
         module = self.ema.module if self.ema else self.model
+        self._set_optimizer_mode('eval')
         test_stats, coco_evaluator = evaluate(module, self.criterion, self.postprocessor,
                 self.val_dataloader, self.evaluator, self.device)
 
